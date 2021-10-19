fb7665b0ff51a230326ae61836a181a65e53dc0f
mdnazmulsobuz <sobuz287@gmail.com>
2021-10-19 13:04:47 +0600

    login from design done

--- src/pages/login/Login/Login.js
+++ src/pages/login/Login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
+import './Login.css';
 
 const Login = () => {
     const {signInUsingGoogle} =useAuth();
@@ -14,9 +16,30 @@ const Login = () => {
         })
     } 
     return (
-        <div>
-            <h2> Here login to me!</h2>
-            <button onClick={handleGoogleLogin} className='btn btn-primary'>Google Sign In </button>
+        <div className='container login-container'>
+            <h2 className='mb-3'> Here login to me!</h2>
+            <div className='row'>
+                <div className='col-md-4'></div>
+                <div className='col-md-4'>
+                <>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Email address"
+                        className="mb-3">
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingPassword" label="Password">
+                        <Form.Control type="password" placeholder="Password" />
+                    </FloatingLabel>
+                    <Button className='my-3 px-4' variant="primary" type="submit">Register</Button>
+                </>
+                    <h6>Or You can signin by Google!</h6>
+                    <div>
+                        <button onClick={handleGoogleLogin} className='btn btn-primary m2-3 px-3'>Google Sign In </button>
+                    </div>
+                </div>
+                <div className='col-md-4'></div>
+            </div>
         </div>
     );
 };
